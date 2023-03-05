585671ed33c3123ce7ae196d200e998d779fb054
Prathamesh Auti <prathamesh16.auti@gmail.com>
2023-03-05 22:43:42 +0530

    Update config.js

--- static/config.js
+++ static/config.js
@@ -2,9 +2,9 @@ const server = {
     port: 5000,
     token_key: "CAPSTONE",
     max_attempts: 2,
-    url: "http://localhost:5000"
+    //url: "http://localhost:5000"
     //url: "https://crazy-fish-tights.cyclic.app"
-    //url: "https://graphical-auth-server.onrender.com"
+    url: "https://graphical-auth-server.onrender.com"
 }
 
 const smtp_settings = {
